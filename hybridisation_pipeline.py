f338a783917dd6e54ca8aa83d3da6d594dfad8f1
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2016-07-10 20:29:20 +0100

    Major refactoring:
    - Stores data in classes rather than dicts
    - Adds wrapper methods to perform all prepocessing steps together
    - Implements the core pipeline in a class HyPred
    - Separate train and predict methods
    - Associates results with the HyPred instance that created them
    - Incorporates results visualization/summarization/analysis into HyPred class

--- hybridisation_pipeline.py
+++ hybridisation_pipeline.py
@@ -16,17 +16,81 @@ import matplotlib.pyplot as plt
 lett_to_num = {'A':1,'T':2, 'G':3, "C":4}
 num_to_lett = {1:'A',2:'T', 3:'G', 4:"C", -9:"X"}
 
+## Data containers
+class TrainData(object):
+    """Class for storing per contig train data"""
+    def __init__(self, X, y, pos, contig, sample_names=None):
+        self.X = X
+        self.y = y
+        self.pos = pos
+        self.contig = contig
+        self.sample_names = sample_names
+        
+class TestData(object):
+    """Class for storing per contig test data"""
+    def __init__(self, X, pos, contig, truth=None, sample_names=None):
+        self.X = X
+        self.pos = pos
+        self.contig = contig    
+        self.truth = truth
+        self.sample_names = sample_names
+        
+class ResultData(object):
+    """Class for storing the per contig results, weak ref'ed to the input data"""
+    def __init__(self, contig, train=None, test=None, cv=None, classifier=None, p0=None):
+        
+        self.train = train
+        self.test = test
+        self.cv = cv
+        self.classifier = classifier
+        self.p0 = p0
+        
+    @property
+    def pred(self):
+        try:
+            return self.__pred
+        except AttributeError:
+            self.__pred = np.empty_like(self.p0, dtype=np.object)
+            
+            for i in range(self.p0.shape[0]):
+                if self.p0[i] > 0.9:
+                    self.__pred[i] = 'popB'
+                elif self.p0[i] < 0.1:
+                    self.__pred[i] = 'popA'
+                else:
+                    self.__pred[i] = 'X'
+            return self.__pred
+                
+
+        
+        
+
+
+
+## Preprocessing
 def load_data(markersA_file, markersB_file, markersAB_file, ref_file):
+    """ Loads SNP data for the two training groups (A, B) and the test group (AB),
+        loads the reference base and constructs a lookup dictionary.
     
-    markersA = pd.read_csv(markersA_file, comment='#', skip_blank_lines=True).drop("group", 1).dropna(axis=0)
-    markersB = pd.read_csv(markersB_file, comment='#', skip_blank_lines=True).drop("group", 1).dropna(axis=0)
+        Params:
+        markersA_file, markersB_file: csv encoded snps for the two ancestral groups
+        markersAB_file: csv encoded snps for the decedent 
+        ref_file: file for locus\treference base
+    
+        Returns:
+        markersA, markersB, markersAB :  filtered dataframe reads of the csvs
+        ref: dictionary mapping locus to reference base
+    """
+    
+    markersA = pd.read_csv(markersA_file,   comment='#', skip_blank_lines=True).drop("group", 1).dropna(axis=0)
+    markersB = pd.read_csv(markersB_file,   comment='#', skip_blank_lines=True).drop("group", 1).dropna(axis=0)
     markersAB = pd.read_csv(markersAB_file, comment='#', skip_blank_lines=True).drop("group", 1).dropna(axis=0)
     
     reference = pd.read_csv(ref_file, comment='#', sep='\t', skip_blank_lines=True)
     ref = { x[0]:str(lett_to_num[x[1]]) for x in reference.to_dict(orient='split')['data']}
     
-    assert np.all(markersA['markers'] == markersB['markers'])
-    assert np.all(markersA['markers'] == markersAB['markers'])
+    assert np.all(markersA['markers'] == markersB['markers']), "The set of loci must be consistent between the groups A, B, AB"
+    assert np.all(markersA['markers'] == markersAB['markers']), "The set of loci must be consistent between the groups A, B, AB"
     
     loci = markersA['markers']
     
@@ -39,13 +103,12 @@ def encode(marker_df, ref, enc_function):
     
         Params:
         marker_df : dataframe of SNPs for each library from Vanessa
-        ref: dictionary mapping contig_pos -> {1,2,3,4} = {A.T, G, C}
+        ref: dictionary mapping contig_pos -> {1,2,3,4} = {A, T, G, C}
         enc_function: function to encode the observed bases relative to the reference args = (ref='2', obs='21')
     
         Returns:
         encoded: np array with shape = (n_loci, n_libraries)
     """
-    
     
     encoded = np.empty((marker_df.shape[0], marker_df.shape[1]-1), dtype=np.object)
     
@@ -78,7 +141,7 @@ def contig_bunch(marker, loci):
         
     return bunched
 
-def create_training_data(bunched1, bunched0):
+def merge_bunched(bunched1, bunched0):
     """ Merges data bunched by contig of the two groups, creates X and y members.
         NB transposes X array so it is (n_libraries, n_markers)
         Params:
@@ -90,96 +153,194 @@ def create_training_data(bunched1, bunched0):
         contig_X: {contig : {'pos':[positions of markers on contig], 'X':array shape (n_libraries1+n_libraries0, n_pos), 'y': array shape (n_libraries1+n_libraries0)}}
     """
     
-    contig_X = {}
+    train_data = {}
     
     for c in bunched0.keys():
         assert bunched1[c]['pos'] == bunched0[c]['pos']
-        contig_X[c] = {}
-        contig_X[c]['pos'] =  bunched0[c]['pos']
-        contig_X[c]['X'] = np.concatenate(( bunched1[c]['data'], bunched0[c]['data']), axis=1)
-        contig_X[c]['X'] = np.transpose(contig_X[c]['X'])
-        contig_X[c]['y'] = np.array([1]*bunched1[c]['data'].shape[1] + [0]*bunched0[c]['data'].shape[1])
         
-    return contig_X
+        train_data[c] = TrainData(X = np.transpose(np.concatenate(( bunched1[c]['data'], bunched0[c]['data']), axis=1)),
+                                  y = np.array([1]*bunched1[c]['data'].shape[1] + [0]*bunched0[c]['data'].shape[1]),
+                                  pos = bunched0[c]['pos'],
+                                  contig = c)
+    return train_data
 
-def create_test_data(bunched):
+def create_training_data(markersA, markersB, encode_func, loci, ref):
+    """Basically a wrapper that combines the data preprocessing stages for the pipeline for the ancestral populations
+        markersA = 1
+        markersB = 0
+    Params:
+    markersA, markersB : dataframes of the snp data for the ancestral populations
+    encode_func: function taking (ref_base, obs) to the feature used to train the classifiers
+    loci: list of loci names as "conitg_pos"
+    ref: dictionary mapping loci to ref_base
     
-    """docstring for create_test_data"""
+    Returns:
+    training_data: dictionary of training data with keyed by contig: {contig : { 'X':matrix of enc snps, 'y': [1,1,1,...,0,0,0,...], 'pos':[physical pos of snp on contig]}}  
+    """
+    data = merge_bunched(contig_bunch(encode(markersA, ref, encode_func), loci), 
+                         contig_bunch(encode(markersB, ref, encode_func), loci))
+    for val in data.values():
+        val.sample_names = list(markersA.columns[1:]) + list(markersB.columns[1:])
+                                    
+    return data
+                                      
+                                      
+def create_test_data(markersAB, encode_func, loci, ref):
+    """Basically a wrapper that combines the data preprocessing stages for the pipeline for the descendent population
+    
+    Params:
+    markersAB: dataframe of the snp data for the descendent population
+    encode_func: function taking (ref_base, obs) to the feature used to train the classifiers
+    loci: list of loci names as "conitg_pos"
+    ref: dictionary mapping loci to ref_base
+        
+    Returns:
+    test_data: dictionary of training data with keyed by contig: {contig : { 'X':matrix of enc snps, 'pos':[physical pos of snp on contig]}}  """
+    
+    bunched = contig_bunch(encode(markersAB, ref, encode_func), loci)
+    
     test_data = {}
     for c in bunched.keys():
-        test_data[c] = {}
-        test_data[c]['pos'] = bunched[c]['pos']
-        test_data[c]['X'] = np.transpose(bunched[c]['data'])
-        
+        test_data[c] = TestData(X = np.transpose(bunched[c]['data']),
+                                pos = bunched[c]['pos'],
+                                contig = c,
+                                sample_names = list(markersAB.columns[1:]))
     return test_data
 
-def train_predict(C, train_data, test_data, plot=False):
-    selected_contigs = list(train_data.keys())
-    
-    classifier = lambda : LogisticRegression(class_weight='balanced', penalty='l1',C=C, fit_intercept=False)
-    print("\n" + '-'*60 + "\n C = "+str(C)+"\n"+'-'*60)
-    
-    cv_accuracy = []
-    cv_selected_contigs = []
-    for contig in train_data.keys():    
-        train_data[contig]['cv'] = cross_validation.cross_val_score(classifier(), X=train_data[contig]['X'], y=train_data[contig]['y'], 
-                                         cv=cross_validation.StratifiedKFold(train_data[contig]['y'], 10, shuffle=True))
-                                         
-        acc = np.mean(train_data[contig]['cv'])
-        cv_accuracy.append(acc)
+
+
+
+## Core pipeline
+class HyPred(object):
+    """docstring for HyPred"""
+    def __init__(self, train_data=None, test_data=None, C=1.0, acc_cutoff=0.95, cv_folds=10, classifier=None):
+        self.train_data = train_data
+        self.test_data = test_data
+        self.C = C
+        self.acc_cutoff = acc_cutoff
+        self.cv_folds = cv_folds
+        self.classifier = classifier
         
-        ## Require CV accuracy of >95% to use to evaluate hybridisation 
-        if acc > 0.95:
-            cv_selected_contigs.append(contig)    
-            train_data[contig]['lr'] = classifier()
-            train_data[contig]['lr'].fit(X=train_data[contig]['X'], y=train_data[contig]['y'])
-    
         
-    print("Successfully trained %i classifiers" % len(cv_selected_contigs))#
+        if self.classifier is None:
+            self.classifier = lambda : LogisticRegression(class_weight='balanced', penalty='l1', C=self.C, fit_intercept=False)
     
-    
-    results = []    
-    results_contigs = {}
-    for contig in cv_selected_contigs:
-        test_data[contig]['p0'] = [x[0] for x in train_data[contig]['lr'].predict_proba(test_data[contig]['X'])]
-        results_contigs[contig] = test_data[contig]
-        results.append(test_data[contig]['p0'])
-        results_contigs[contig]['lr'] = train_data[contig]['lr']
-    
-    results = np.array(results)
-    
-    print(">0.9 : " + ' '.join([str(x) for x in np.sum(results > 0.9, axis=0)]))
-    print("<0.1 : " + ' '.join([str(x) for x in np.sum(results < 0.1, axis=0)]))
-    
-    
-    if plot==True:
-        plt.figure()
-        plt.hist(cv_accuracy, bins=50)
-        plt.figure()
-        for i in range(results.shape[1]):
-            plt.hist(results[:,i],bins=50, histtype='step')
-            plt.xlabel("Pr(Group 4)")
+    def train(self):
+        
+        """For each contig in train_data train a classifier and do cv_fold cross validation and select 
+            contigs which can be classified with accuracy greater than acc_cutoff. Re-train on these 
+            selected contigs with all of the training data and update train_data with the trained classifiers
+        
+            Params:
+            train_data : output of create_training_data
+            C: L1 regularisation parameter for the default LogisticRegression classifier
+            acc_cutoff : Minimum cross validation accuracy for selection
+            cv_folds: Number of cross validation folds to use, cannot exceed number of samples in the smallest ancestral population
+            classifier : classifier factory to use, defaults to  LogisticRegression(class_weight='balanced', penalty='l1',C=C, fit_intercept=False) 
+        
+            Returns:
             
-    return results_contigs
-
-def plot(pssm, cb=True):
-    '''
-    Plots the S matrix
-    '''     
-    pssm = np.array(pssm, dtype=np.float)
-    plt.matshow(pssm, cmap = cm.seismic, vmax=abs(pssm).max(), vmin=-abs(pssm).max())
-    if cb:
-        plt.colorbar()
-    plt.show()
-
-
-def examine_contig(contig, train, result):
-    plot(result[contig]['X'], cb=False)
-    plt.title("Group 41")
+            train_data: updated with trained classifiers 'classifier' and their cross validated accuracy 'cv' 
+            cv_selected_contigs: list of selected contig names"""
+        
+                
+        self.results_data = {}
+        self.selected_contigs = []
+        
+        for contig in self.train_data.keys():    
     
-    plot(train[contig]['X'], cb=False)
-    plt.title("Group 1 and Group 4")
+            cv = cross_validation.cross_val_score(self.classifier(), 
+                                                  X=self.train_data[contig].X, 
+                                                  y=self.train_data[contig].y, 
+                                                  cv=cross_validation.StratifiedKFold(self.train_data[contig].y, self.cv_folds, shuffle=True))
+                                                  
+            ## Require self.CV accuracy of >self.acc_cutoff to use to evaluate hybridisation 
+            if np.mean(cv) > self.acc_cutoff:
+                self.selected_contigs.append(contig)
+                self.results_data[contig] = ResultData(train=self.train_data[contig],
+                                                  cv=cv,
+                                                  contig=contig,   
+                                                  classifier = self.classifier())
+                self.results_data[contig].classifier.fit(X=self.train_data[contig].X, y=self.train_data[contig].y)
+                
+        print("Successfully trained %i classifiers" % len(self.selected_contigs))#
+        
+        return len(self.selected_contigs)
+        
+    def predict(self):
+        """Predict ancestral population of origin, using test_data and the trained
+         classifiers in results data, for the contigs in selected_contigs
+        
+        Params:
+        results_data: dictionary {contig : ResultData} where the ResultData object has a classifier member populated by a trained classifier """
+        
+        self.pred_matrix = []
+        self.p0_matrix = []
+        for contig in self.selected_contigs:
+            self.results_data[contig].test = self.test_data[contig]
+            self.results_data[contig].p0 = np.array([x[0] for x in self.results_data[contig].classifier.predict_proba(self.test_data[contig].X)])
+            
+            self.pred_matrix.append(self.results_data[contig].pred)
+            self.p0_matrix.append(self.results_data[contig].p0)
+            
+        self.pred_matrix = np.array(self.pred_matrix)
+        self.p0_matrix = np.array(self.p0_matrix)
+        
+        
+    def plot(self):
+        """Plot the distribution of ancestral probabilities"""
+        sample_names = next(iter(self.test_data.values())).sample_names
+        plt.figure()
+        for i in range(self.p0_matrix.shape[1]):
+            plt.hist(self.p0_matrix[:,i], bins=50, histtype='step', label=sample_names[i])
+            plt.xlabel("Pr(Group B)")
+        plt.legend()
+        plt.show()
     
-    plot(result[contig]['lr'].coef_, cb=False)
-    plt.title("Classifier weights")
+    def summarise(self):
+        """Summary table of ancestral predictions"""
+        sample_names = next(iter(self.test_data.values())).sample_names
+        
+        A = np.sum(self.pred_matrix == "popA", axis=0)
+        B = np.sum(self.pred_matrix == "popB", axis=0)
+        X = np.sum(self.pred_matrix == "X", axis=0)
+        ratio = A/B
+        
+        print("Sample  \tpopA\tpopB\tno pred\tratio")
+        print("-"*60)
+        for i in range(len(sample_names)):
+            print("{0}\t{1}\t{2}\t{3}\t{4:.2f}".format(sample_names[i], A[i], B[i], X[i], ratio[i]))
     
+
+    def examine_contig(self, contig=None):
+        if contig is None:
+            contig = next(iter(self.results_data.keys()))
+            
+        print("Contig {0}\n".format(contig))
+        
+        print("Sample  \tPrediction\t Pr(B)")
+        print("-"*40)
+        for s,p, p0 in zip(self.test_data[contig].sample_names, self.results_data[contig].pred, self.results_data[contig].p0  ):
+            print("{0}\t{1}\t{2:.2f}".format(s,p,p0))
+            
+        print("\nCV Accuracy: {0:.2f}".format(np.mean(self.results_data[contig].cv)))
+        
+        plt.matshow(np.array(self.train_data[contig].X, dtype=np.float), vmax=1, vmin=-1, cmap=cm.seismic)
+        plt.yticks(range(len(self.train_data[contig].sample_names)), self.train_data[contig].sample_names, size=4)
+        plt.xticks(range(len(self.train_data[contig].pos)), self.train_data[contig].pos, rotation=90, size=4)
+        plt.title("Training data")
+        
+        plt.matshow(np.array(self.test_data[contig].X, dtype=np.float), vmax=1, vmin=-1, cmap=cm.seismic)
+        plt.yticks(range(len(self.test_data[contig].sample_names)), self.test_data[contig].sample_names, size=4)
+        plt.xticks(range(len(self.test_data[contig].pos)), self.test_data[contig].pos, rotation=90, size=4)
+        plt.title("Test data")
+        
+        plt.matshow(np.array(self.results_data[contig].classifier.coef_, dtype=np.float), 
+                            vmax=abs(self.results_data[contig].classifier.coef_).max(), 
+                            vmin=-abs(self.results_data[contig].classifier.coef_).max(), 
+                            cmap=cm.seismic)
+        plt.xticks(range(len(self.test_data[contig].pos)), self.test_data[contig].pos, rotation=90, size=4)
+        plt.title("Weight matrix")
+    
+
+
--- pipeline_test.py
+++ pipeline_test.py
@@ -1,79 +1,19 @@
 #!/usr/bin/env python
-from hybridisation_pipeline import (encode, contig_bunch, create_training_data, # Methods
-                                    create_test_data, train_predict, plot, examine_contig, load_data) 
-                                    
-                                    
-from snp_encodings import discard_het, half_count, label 
+
+from hybridisation_pipeline import load_data, create_training_data, create_test_data, HyPred  
+from snp_encodings import discard_het
 
 import numpy as np
 import matplotlib.pyplot as plt 
-"""Label Group 1 = 1
-         Group 4 = 0"""
-
-markersA, markersB, markersAB, ref, loci = load_data("markers_1.txt", "markers_4.txt", "markers_41.txt", "reference.txt")
-
-################## half_count ######################################################
-marker_1_enc = encode(markers_1, ref, half_count)
-marker_4_enc = encode(markers_4, ref, half_count)            
-hybrid_enc = encode(markers_41, ref, half_count)            
-            
-marker_1_bunched = contig_bunch(marker_1_enc, loci)
-marker_4_bunched = contig_bunch(marker_4_enc, loci)
-hybrid_bunched = contig_bunch(hybrid_enc, loci)
-
-unfiltered = create_training_data(marker_1_bunched, marker_4_bunched)
-hc_test_data = create_test_data(hybrid_bunched)
-hc_training_data = {k:v for k,v in unfiltered.items() if np.sum(np.abs(v['X'])) > 1000}
-
- 
-## Now select contigs that look reasonable,
-## Filter for contig
-
-snps_per_contig = [contig['X'].shape[1] for contig in unfiltered.values()]
-score_per_contig = [np.sum(np.abs(contig['X'])) for contig in unfiltered.values()]
 
 
-plt.hist(score_per_contig, bins=25)
-plt.savefig("score_per_contig.pdf")
-plt.close()
+"""Label Group 1 = 1 = A
+         Group 4 = 0 = B"""
 
-plt.hist(snps_per_contig, bins=25)
-plt.savefig("snps_per_contig.pdf")
-plt.close()
+markersA, markersB, markersAB, ref, loci = load_data("data/markers_1.txt", "data/markers_4.txt", "data/markers_41.txt", "data/reference.txt")
+training_data = create_training_data(markersA, markersB, discard_het, loci, ref)
+test_data  = create_test_data(markersAB, discard_het, loci, ref) 
 
-
-
-########################################################################
-
-
-################## discard het ######################################################v
-
-
-
-unfiltered = create_training_data(contig_bunch(encode(markers_1, ref, discard_het), loci), 
-                                  contig_bunch(encode(markers_4, ref, discard_het), loci))
-
-dh_training_data = {k:v for k,v in unfiltered.items() if np.sum(np.abs(v['X'])) > 1000}
-dh_test_data = create_test_data(contig_bunch(encode(markers_41, ref, discard_het), loci))
-
-
-snps_per_contig = [contig['X'].shape[1] for contig in unfiltered.values()]
-score_per_contig = [np.sum(np.abs(contig['X'])) for contig in unfiltered.values()]
-
-plt.hist(score_per_contig, bins=25)
-plt.savefig("score_per_contig.pdf")
-plt.close()
-
-plt.hist(snps_per_contig, bins=25)
-plt.savefig("snps_per_contig.pdf")
-plt.close()
-
-
-
-
-train_predict(1.0, dh_training_data, dh_test_data)
-
-
-##########################################################################################
-
-
+hp = HyPred(train_data=training_data, test_data=test_data, C=0.75)
+hp.train()
+hp.predict()
