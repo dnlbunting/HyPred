d88d3a73e49716552908f13a7f3115b887a22cf3
Daniel Bunting <daniel.bunting14@imperial.ac.uk>
2016-07-10 22:41:00 +0100

    Update simulation pipeline to match refactored core code

--- simulations/simulation_pipeline.py
+++ simulations/simulation_pipeline.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from hybridisation_pipeline import (encode, contig_bunch, create_training_data, create_test_data, train_predict, plot, examine_contig, load_data)
-from snp_encodings import discard_het, half_count, label
+from hybridisation_pipeline import load_data, create_training_data, create_test_data, HyPred  
+from snp_encodings import discard_het
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,70 +10,54 @@ import pandas as pd
 """Label Group A = 1
          Group B = 0"""
 
-def compare_to_truth(results, recombmap_file):
+def compare_to_truth(hp):
     
-    truth_df = pd.read_csv(recombmap_file, index_col=0)
-    
-    for contig in results.keys():
-        results[contig]['pred'] = []
-        for lib in range(len(results[contig]['p0'])):
-            if results[contig]['p0'][lib] < 0.1 :
-                results[contig]['pred'].append('popA')
-            elif results[contig]['p0'][lib] > 0.9 :
-                results[contig]['pred'].append('popB')
-            else:
-                results[contig]['pred'].append('X')
-        results[contig]['pred'] = np.array(results[contig]['pred'])
         
-        
-    print("{} contigs tested".format(len(list(results.keys()))*7))
+    print("{} contigs tested".format( len(list(hp.results_data.keys())) *  len(next(iter(hp.test_data.values())).sample_names) ))
     
-    print ("Contigs not assigned {0}".format(np.sum([np.sum(x['pred'] == np.array('X')) for x in results.values()])))
+    print ("Contigs not assigned {0}".format( np.sum([ np.sum(x.pred == 'X') for x in hp.results_data.values()]) ))
     
-    print ("Contigs correctly identified as popA {0}".format((
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popA']),
-                                                       results[contig]['pred'] == np.array(['popA'])))) 
-                                                      for contig in results.keys()]))))
+    print ("Contigs correctly identified as popA {0}".format(
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popA'],
+                                                       hp.results_data[contig].pred == ['popA']))) 
+                                                       for contig in hp.results_data.keys()] )))
                                     
     print ("Contigs from popA misidentified {0}".format(
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popA']),
-                                                       results[contig]['pred'] == np.array(['popB'])))) 
-                                                       for contig in results.keys()])))
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popA'],
+                                                       hp.results_data[contig].pred == ['popB']))) 
+                                                       for contig in hp.results_data.keys()] )))
     print ("Contigs from popA unidentified {0}".format(
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popA']),
-                                                       results[contig]['pred'] == np.array(['X'])))) 
-                                                       for contig in results.keys()])))
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popA'],
+                                                       hp.results_data[contig].pred == ['X']))) 
+                                                       for contig in hp.results_data.keys()] )))
                                                        
     
-    print ("Contigs correctly identified as popB {0}".format((
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popB']),
-                                                       results[contig]['pred'] == np.array(['popB'])))) 
-                                                      for contig in results.keys()]))))
+    print ("Contigs correctly identified as popB {0}".format(
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popB'],
+                                                       hp.results_data[contig].pred == ['popB']))) 
+                                                       for contig in hp.results_data.keys()] )))
                                     
     print ("Contigs from popB misidentified {0}".format(
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popB']),
-                                                       results[contig]['pred'] == np.array(['popA'])))) 
-                                                       for contig in results.keys()])))
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popB'],
+                                                       hp.results_data[contig].pred == ['popA']))) 
+                                                       for contig in hp.results_data.keys()] )))
     print ("Contigs from popB unidentified {0}".format(
-                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == np.array(['popB']),
-                                                       results[contig]['pred'] == np.array(['X'])))) 
-                                                       for contig in results.keys()])))
+                        np.sum([(np.sum(np.logical_and(np.array(truth_df.ix[int(contig)]) == ['popB'],
+                                                       hp.results_data[contig].pred == ['X']))) 
+                                                       for contig in hp.results_data.keys()] )))
+
+
+
+def load_truth(hp, recombmap_file):
+    truth_df = pd.read_csv(recombmap_file, index_col=0)
+    for contig in hp.test_data.keys():
+        hp.test_data[contig].truth = np.array(truth_df.ix[int(contig)])
+        
 
 
 markersA, markersB, markersAB, ref, loci = load_data("simulations/popA.csv", "simulations/popB.csv", "simulations/popAB.csv", "simulations/reference.txt")
-unfiltered = create_training_data(contig_bunch(encode(markersA, ref, discard_het), loci),
-                                  contig_bunch(encode(markersB, ref, discard_het), loci))
-
-snps_per_contig = [contig['X'].shape[1] for contig in unfiltered.values()]
-score_per_contig = [np.sum(np.abs(contig['X'])) for contig in unfiltered.values()]
-
-plt.hist(score_per_contig, bins=25)
-plt.hist(snps_per_contig, bins=25)
-
-
-
-training_data = {k:v for k,v in unfiltered.items() if np.sum(np.abs(v['X'])) > 1000}
-test_data = create_test_data(contig_bunch(encode(markersAB, ref, discard_het), loci))
-results = train_predict(1.0, training_data, test_data,plot=True)
-compare_to_truth(results, "simulations/popAB.recombmap")
-
+training_data = create_training_data(markersA, markersB, discard_het, loci, ref)
+test_data  = create_test_data(markersAB, discard_het, loci, ref) 
+hp = HyPred(train_data=training_data, test_data=test_data, C=1.)
+hp.train()
+hp.predict()
