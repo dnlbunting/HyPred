013fa95e6ee79ca6356f3435985bb83582363041
Daniel Bunting (TGAC) <buntingd@n80079.nbi.ac.uk>
2016-07-14 11:24:40 +0100

    Major refactoring of simulator code. Basically able to do the same things as the original code now with working contig level recombination of haploid populations

--- simulations/simulate_snps.py
+++ simulations/simulate_snps.py
@@ -2,25 +2,230 @@
 
 import pandas as pd
 import numpy as np
+import copy, os
+
 num_to_lett = {1:'A',2:'T', 3:'G', 4:"C", -9:"X"}
 
-def create_referece(n_contigs, markers_mean, prefix=""):
-    loci, base, contig_mask = [], [], []
-    
-    markers_per_contig = np.array(np.floor(np.random.exponential(markers_mean, size=n_contigs)), dtype=np.int)
-    
-    for contig in range(n_contigs):
-          loci += [prefix+str(contig)+'_'+str(i) for i in range(markers_per_contig[contig])]
-          base += list(np.random.randint(1,5,markers_per_contig[contig]))
-          contig_mask +=[contig]*markers_per_contig[contig]
-          
-    return loci, base, markers_per_contig
+@np.vectorize              
 def mut(a,b):
     "mutate base a by amount b"
     if a == -9:
         return a
     else:
         return (a+b)%4 + 1
+        
+        
+        
+class Individual(object):
+    """docstring for Individual"""
+
+    def __init__(self, reference, genomeA=None, genomeB=None, ploidy='N', lineage=None, rng=None):
+        self.reference = reference
+        self.ploidy = ploidy
+        self.genomeA = genomeA
+        self.genomeB = genomeB
+        self.lineage = lineage
+        self.rng = rng
+        
+        if self.rng is None:
+            self.rng = np.random.RandomState()
+            
+        if self.genomeA is None:
+            self.genomeA = copy.deepcopy(self.reference)
+        
+        # Default polyploid indvidual to have to identical genomes
+        if self.ploidy == 'N+N' or self.ploidy == '2N':
+            if self.genomeB is None:
+                self.genomeB = copy.deepcopy(self.genomeA)
+                
+    def __deepcopy__(self, memo):
+        cls = self.__class__
+        result = cls.__new__(cls)
+        memo[id(self)] = result
+        
+        
+        ## Deep copy everything...
+        for k, v in self.__dict__.items():
+            setattr(result, k, copy.deepcopy(v, memo))
+            
+        ## Except the reference and the rng (very important)
+        setattr(result, 'reference', self.reference)
+        setattr(result, 'rng', self.rng)
+        
+        return result
+        
+                
+    def mutate(self, mutation_mean, mutation_sd):
+        """docstring for _mutateIndiv"""
+        if self.ploidy == 'N':
+            n_mut = np.array(np.abs(self.rng.normal(mutation_mean, mutation_sd)), dtype=np.int)
+            mutation = np.array(self.rng.randint(0,3,n_mut), dtype=np.int)
+            position = np.array(self.rng.randint(0,len(self.genomeA)-1, n_mut), dtype=np.int)
+            self.genomeA[position] = mut(self.genomeA[position], mutation)
+            
+        else:
+            n_mut = np.array(np.abs(self.rng.normal(mutation_mean, mutation_sd, 2)), dtype=np.int)
+            
+            mutationA = np.array(self.rng.randint(0,3,n_mut[0]), dtype=np.int)
+            mutationB = np.array(self.rng.randint(0,3,n_mut[1]), dtype=np.int)
+            
+            positionA = np.array(self.rng.randint(0,len(self.genomeA)-1, n_mut[0]), dtype=np.int)
+            positionB = np.array(self.rng.randint(0,len(self.genomeB)-1, n_mut[1]), dtype=np.int)
+            
+            self.genomeA[positionA] = mut(self.genomeA[positionA], mutationA)
+            self.genomeB[positionB] = mut(self.genomeB[positionB], mutationB)
+
+class Population(object):
+    """docstring for Population"""
+    def __init__(self, individuals, name, rng=None, reference=None):
+        self.individuals = individuals
+        self.name = name
+        self.rng = rng
+        self.reference = reference
+        
+        if self.rng is None:
+            self.rng = np.random.RandomState()
+        
+    def mutate(self, mutation_mean, mutation_sd):
+        """docstring for mutate"""
+        for indv in self.individuals:
+            indv.mutate(mutation_mean, mutation_sd)
+            
+    def write(self):
+        """docstring for write"""
+        contig_loci = ["{0}_{1}".format(c,l) for c,l in zip(self.reference['contig'], self.reference['loci'])]
+        
+        if self.individuals[0].ploidy == 'N':
+            pop = np.vstack([indv.genomeA for indv in self.individuals])
+            for i in range(pop.shape[0]):
+                for j in range(pop.shape[1]):
+                    pop[i,j] = str(pop[i,j])*2
+                    
+        else:
+            genA = np.vstack([indv.genomeA for indv in self.individuals])
+            genB = np.vstack([indv.genomeB for indv in self.individuals])
+            pop = np.empty_like(genA)
+        
+            for i in range(pop.shape[0]):
+                for j in range(pop.shape[1]):
+                    pop[i,j] = str(genA[i,j])+str(genB[i,j])
+                    
+        df = pd.DataFrame( np.vstack( ([self.name]*len(contig_loci), contig_loci, pop)).T, columns = ['group', 'markers']+['sample'+str(i) for i in range(pop.shape[0])])
+        df.to_csv(self.name+'.csv', index=False)
+        
+    @property
+    def n_individuals(self):
+        return len(self.individuals)
+
+class Simulation(object):
+    """docstring for Simulation"""
+    def __init__(self, reference=None, random_seed=42, ploidy='N'):
+        self.reference = reference
+        self.rng = np.random.RandomState(random_seed)
+        self.populations = {}
+        self.ploidy = ploidy
+    
+    def create_reference(self, n_contigs, markers_mean, prefix=""):
+        
+        self.markers_per_contig = np.array(self.rng.exponential(markers_mean, size=n_contigs), dtype=np.int) + 1
+        self.reference = {}
+        
+        self.reference['loci'] = np.zeros(np.sum(self.markers_per_contig), dtype=np.int)
+        self.reference['base'] = np.zeros_like(self.reference['loci'], dtype=np.int)
+        self.reference['contig'] = np.zeros_like(self.reference['loci'], dtype=np.int)
+        
+        idx=0
+        for contig in range(n_contigs):
+            old_idx = idx
+            idx += self.markers_per_contig[contig]
+            self.reference['loci'][old_idx:idx] = np.arange(self.markers_per_contig[contig])
+            self.reference['base'][old_idx:idx] = np.random.randint(1,5,self.markers_per_contig[contig])
+            self.reference['contig'][old_idx:idx] = contig
+        
+        self.populations['reference'] = Population([Individual(genomeA=copy.deepcopy(self.reference['base']),
+                                                               reference=self.reference,
+                                                               ploidy=self.ploidy,
+                                                               rng=self.rng)], name='reference', rng=self.rng, reference=self.reference)
+    
+    def divide(self, in_pop, sizes, out_pops=None, replace=True):
+        """Either divides a population of individuals in_pop into new populations 
+          out_pops each with the number individuals in the corresponding sizes element 
+        or if no out_pop is specified divides in_pop into sizes new individuals"""
+        
+        if out_pops is None:
+            out_pops = [in_pop]
+            sizes = [sizes]
+            
+        assert len(out_pops) == len(sizes), "All subpopulations must have sizes defined"
+        if replace is False:
+            assert self.populations[in_pop].n_individuals >= np.sum(sizes), "To sample without replacement need more individuals in the in_pop than in all the out_pops"
+            
+        in_pop = self.populations[in_pop]
+        del self.populations[in_pop.name]
+        
+        for i,pop in enumerate(out_pops):
+            choices = self.rng.choice(range(in_pop.n_individuals),
+                                      size=sizes[i], 
+                                      replace=replace)
+            copied_indvs = [copy.deepcopy(in_pop.individuals[c]) for c in choices]
+            self.populations[pop] = Population(copied_indvs, name=pop, rng=self.rng, reference=self.reference)
+    
+    def exchangeNuclei(self, popA, popB, popAB, n_children):
+        """Create n_children individuals with genomeA coming from a random 
+        individual in popA and genomeB from an individual in popB"""
+        
+        parentsA = self.rng.choice(self.populations[popA].individuals, size=n_children)
+        parentsB = self.rng.choice(self.populations[popB].individuals, size=n_children)
+        self.populations[popAB] = Population([], name=popAB, rng=self.rng, reference=self.reference)
+        
+        for i in range(n_children):
+            hybrid = copy.deepcopy(parentsA[i])
+            hybrid.genomeB = copy.deepcopy(parentsB[i].genomeB)
+            
+            self.populations[popAB].individuals.append(hybrid)
+    
+    def recombineContigs(self, popA, popB, popAB, n_children, n_parentsA=None, n_parentsB=None, ratio=0.5):
+        """docstring for recombine"""
+        if n_parentsA is None:
+            n_parentsA = len(self.populations[popA].individuals)
+        if n_parentsB is None:
+            n_parentsB = len(self.populations[popB].individuals)
+            
+        parentsA = self.rng.choice(self.rng.choice(self.populations[popA].individuals, size=n_parentsA, replace=False), size=n_children)
+        parentsB = self.rng.choice(self.rng.choice(self.populations[popB].individuals, size=n_parentsB, replace=False), size=n_children)
+
+        self.populations[popAB] = Population([], name=popAB, rng=self.rng, reference=self.reference)
+        
+        if self.ploidy == 'N':
+            for i in range(n_children):
+                gen = self.recombineHaploid(parentsA[i].genomeA, parentsB[i].genomeA, ratio=ratio)
+                self.populations[popAB].individuals.append(Individual(genomeA=np.copy(gen), 
+                                                                      reference=self.reference,
+                                                                      ploidy=self.ploidy,
+                                                                      rng=self.rng))
+
+            
+    def recombineHaploid(self, genA, genB, ratio ):
+        """docstring for recombineHaploid"""
+        parent_mask = []
+        parent = self.rng.uniform(size=len(self.markers_per_contig)) > ratio
+        for contig,_ in enumerate(self.markers_per_contig):
+            parent_mask += [parent[contig] for i in range(self.markers_per_contig[contig])]
+        return np.where(parent_mask, genA, genB)
+    
+    def write(self, folder):
+        """docstring for write"""
+        print("Writing reference to {0}".format(os.path.join(folder, "reference.txt")))
+        
+        with open(os.path.join(folder, "reference.txt"), 'w') as f:
+            f.write("marker\tref\n")
+            for l,c, b in zip(self.reference['loci'], self.reference['contig'], self.reference['base']):
+                f.write("{0}_{1}\t{2}\n".format(c, l, num_to_lett[b]))
+        
+        for pop in self.populations.values():
+            print("Writing population {0}".format(pop.name))
+            pop.write()
+
 def diverge(reference, mutation_mean, mutation_sd, n_children):
     
     children = [np.copy(reference) for i in range(n_children)]
