4d1147671b8b7a28968ae3c67597b5f1e0959454
Daniel Bunting (TGAC) <buntingd@n80079.nbi.ac.uk>
2016-07-29 13:26:14 +0100

    Adds missingData to Simulator

--- simulations/simulate_snps.py
+++ simulations/simulate_snps.py
@@ -50,7 +50,7 @@ class Individual(object):
         for k, v in self.__dict__.items():
             setattr(result, k, copy.deepcopy(v, memo))
             
-        ## Except the reference and the rng (very important)
+        ## Except the reference and the rng (very important!!!!!!)
         setattr(result, 'reference', self.reference)
         setattr(result, 'rng', self.rng)
         
@@ -118,7 +118,7 @@ class Population(object):
             genA = np.vstack([indv.genomeA for indv in self.individuals])
             genB = np.vstack([indv.genomeB for indv in self.individuals])
             
-            pop = np.empty_like(genA)
+            pop = np.empty_like(genA, dtype=np.object)
         
             for i in range(pop.shape[0]):
                 for j in range(pop.shape[1]):
@@ -143,9 +143,16 @@ class Population(object):
     def stats(self):
         """docstring for stats"""
         print("\nPopulation {0} statistics\n".format(self.name) + "-"*60 )
+        
         het = [np.sum(x.genomeA != x.genomeB)/float(len(x.genomeA)) for x in self.individuals]
-        print("Heterokaryotic ratio {0}".format("\t".join(["{0:.3f}".format(x) for x in  het])))
+        missing = [np.sum(np.logical_or(x.genomeA == -9, x.genomeB == -9))/float(len(x.genomeA)) for x in self.individuals]
+        
+        #print("Heterokaryotic ratio {0}".format("\t".join(["{0:.3f}".format(x) for x in  het])))
         print("Mean heterokaryotic ratio {0}".format(np.mean(het)))
+        
+        #print("Missing SNPs {0}".format("\t".join(["{0:.3f}".format(x) for x in  missing])))
+        print("Mean missing SNPs {0}".format(np.mean(missing)))
+        
         print("-"*60)
 
     @property
@@ -154,7 +161,7 @@ class Population(object):
 
 class Simulation(object):
     """docstring for Simulation"""
-    def __init__(self, reference=None, random_seed=42, ploidy='N'):
+    def __init__(self, reference=None, random_seed=None, ploidy='N'):
         self.reference = reference
         self.rng = np.random.RandomState(random_seed)
         self.populations = {}
@@ -288,119 +295,14 @@ class Simulation(object):
         for pop in self.populations.values():
             print("Writing population {0}".format(pop.name))
             pop.write()
+    
+    def missingData(self, popA, rate_mean, rate_sd):
+        """Simulate missing data in the individuals of popA such 
+        that a fraction rate of the SNP sites are set to -9-9"""
+        n_errs = np.array(np.floor(len(self.reference['loci']) * np.abs(self.rng.normal(rate_mean, rate_sd, self.populations[popA].n_individuals))), dtype=np.int)
 
-def diverge(reference, mutation_mean, mutation_sd, n_children):
+        for i,indiv in enumerate(self.populations[popA].individuals):
+            position = self.rng.choice(np.arange(0, len(indiv.genomeA)), n_errs[i], replace=False)
+            indiv.genomeA[position] = -9
+            indiv.genomeB[position] = -9
     
-    children = [np.copy(reference) for i in range(n_children)]
-    n_mut = np.array(np.floor(np.abs(np.random.normal(mutation_mean, mutation_sd, n_children))), dtype=np.int)
-    
-    for i in range(n_children):
-        mutation = np.array(np.random.randint(0,3,n_mut[i]), dtype=np.int)
-        position = np.array(np.random.randint(0,len(reference)-1, n_mut[i]), dtype=np.int)
-        
-        for j in range(n_mut[i]):
-            children[i][position[j]] = mut(children[i][position[j]], mutation[j])
-    return children    
-def recombineIndiv(indA, indB, ratio, markers_per_contig):
-    
-    parent = np.random.uniform(size=len(markers_per_contig)) > ratio
-    parent_mask = []
-    for contig in range(len(markers_per_contig)):
-        parent_mask += [parent[contig] for i in range(markers_per_contig[contig])]
-        
-    return np.where(parent_mask, indA, indB), np.where(parent,'popA', 'popB')
-        
-def recombine(popA, popB, ratio, markers_per_contig, n_children):
-    parentsA = np.random.randint(0,len(popA)-1, n_children)
-    parentsB = np.random.randint(0,len(popB)-1, n_children)
-    res = [recombineIndiv(popA[a],popB[b], ratio, markers_per_contig) for a,b in zip(parentsA, parentsB)]
-    return [x[0] for x in res], [x[1] for x in res]
-    
-def writeMarkers(pop, loci, name):
-    pop = np.array(pop, dtype=np.object)
-    for i in range(pop.shape[0]):
-        for j in range(pop.shape[1]):
-            pop[i,j] = str(pop[i,j])*2
-            
-    df = pd.DataFrame( np.vstack( ([name]*len(loci), loci, pop)).T, columns = ['group', 'markers']+['sample'+str(i) for i in range(pop.shape[0])])
-    df.to_csv(name+'.csv', index=False)
-    
-def writeReference(loci, base, name):
-    with open(name, 'w') as f:
-        f.write("marker\tref\n")
-        for l,b in zip(loci, base):
-            f.write(str(l)+"\t"+num_to_lett[b]+"\n")
-
-def writeRecombMap(recombMap, name):
-    with open(name+".recombmap",'w') as f:
-        f.write('contig,'+ ','.join(['sample'+str(i) for i in range(len(recombMap))]) +'\n' )
-        for i,m in enumerate(np.transpose(recombMap)):
-            f.write("{0},{1}\n".format(i,','.join(m)))
-        
-            
-                
-def simulate_missing_data(pop, rate_mean, rate_sd):
-    
-    n_errs = np.array(np.floor(len(pop[0])*np.abs(np.random.normal(rate_mean, rate_sd, len(pop)))), dtype=np.int)
-    for i in range(len(pop)):
-         position = np.array(np.random.randint(0,len(pop[i])-1, n_errs[i]), dtype=np.int)
-         for j in range(n_errs[i]):
-             pop[i][position[j]] = -9
-             
-    return pop
-
-    
-
-
-## From the dataset:
-## n_contigs =5000
-## mean_markers_per_contig = 7.5
-## mean 
-
-def simulate_hybrid():
-    
-    loci, base, markers_per_contig = create_referece(5000,7.5)
-    a, b = diverge(base, 18000, 2500, 2)
-    
-    ## Simulate lineage specific deletions 
-    a, = simulate_missing_data([a], 0.1,0.05)
-    b, = simulate_missing_data([b], 0.1,0.05)
-    
-    
-    popA = diverge(a, 1000, 250, 25)
-    popB = diverge(b, 1000, 250, 25)
-    
-    popC, recombination_map = recombine(popA, popB, 0.1, markers_per_contig, 7)
-    popC = diverge(c, 1000, 250, 10)
-    
-    popA = simulate_missing_data(popA, 0.5,0.05)
-    popB = simulate_missing_data(popB, 0.5,0.05)
-    popC = simulate_missing_data(popC, 0.5,0.05)
-    
-    writeRecombMap(recombination_map, 'popAB')
-    writeMarkers(popA, loci, "popA")
-    writeMarkers(popB, loci, "popB")
-    writeMarkers(popC, loci, "popAB")
-    writeReference(loci, base, "reference.txt")
-
-
-def simulate_no_hybrid():
-    loci, base, markers_per_contig = create_referece(5000,7.5)
-    a, b = diverge(base, 18000, 2500, 2)
-    b,c = diverge(b, 10000,2500, 2)
-    
-    popA = diverge(a, 1000, 250, 34)
-    popB = diverge(b, 1000, 250, 100)
-    popC = diverge(c, 1000, 250, 10)
-    
-    popA = simulate_missing_data(popA, 0.5,0.05)
-    popB = simulate_missing_data(popB, 0.5,0.05)
-    popC = simulate_missing_data(popC, 0.5,0.05)
-    
-    
-    writeMarkers(popA, loci, "popA")
-    writeMarkers(popB, loci, "popB")
-    writeMarkers(popC, loci, "popAB")
-    writeReference(loci, base, "reference.txt")
-
-
